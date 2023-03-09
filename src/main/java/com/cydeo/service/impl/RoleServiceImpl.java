317fe61030ff35c7ff9e08103526fc39d49301f6
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-09 15:50:23 -0500

    add Component annotation

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,9 +2,10 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService {
     @Override
     public RoleDTO save(RoleDTO user) {
