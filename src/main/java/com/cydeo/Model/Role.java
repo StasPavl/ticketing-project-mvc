03cefacc362d9dbb74832abf5a53afdd85fdaffb
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-08 19:32:53 -0500

    Create models User and Role

---
+++ src/main/java/com/cydeo/Model/Role.java
@@ -0,0 +1,14 @@
+package com.cydeo.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    private Long id;
+    private String description;
+}
---
+++ src/main/java/com/cydeo/Model/User.java
@@ -0,0 +1,11 @@
+package com.cydeo.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+}
