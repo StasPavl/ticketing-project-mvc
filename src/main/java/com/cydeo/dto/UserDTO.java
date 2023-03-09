09de0dc12ec822595b540b1afad602a7d29adf78
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-09 18:13:09 -0500

    change from role to roleDTO in the veriables

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -16,6 +16,6 @@ public class UserDTO {
     private String passWord;
     private boolean enabled;
     private String phone;
-    private Role role;
+    private RoleDTO role;
     private Gender gender;
 }
