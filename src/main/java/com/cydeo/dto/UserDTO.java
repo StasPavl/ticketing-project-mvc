896797107129ee5927e1269750d34fdda390279e
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-08 19:44:35 -0500

    DTO classes created (USerDTO and RoleDTO)

---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.cydeo.dto;
+
+import com.cydeo.Model.Role;
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+}
