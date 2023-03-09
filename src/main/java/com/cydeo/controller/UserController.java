aa0580def63a19e9f84b05cdfebfe044a4a00b11
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-09 15:51:00 -0500

    did dependency injection and added list with roles to my model

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+        private final RoleServiceImpl roleService;
+
+        public UserController(RoleServiceImpl roleService) {
+                this.roleService = roleService;
+        }
 
         @GetMapping("/create")
         public String createUser(Model model){
 
                 model.addAttribute("user", new UserDTO());
+                model.addAttribute("listRoles",roleService.findAll());
         return "/user/create";
         }
 }
