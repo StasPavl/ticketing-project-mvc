6b0081e62753da32b7a94bc96e2fc178186909b2
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-14 00:58:05 -0400

    Project Controller created, function to create new project and function to show all project in Project List, and Delete function added

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,49 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers", userService.findAll());
+        model.addAttribute("projects",projectService.findAll());
+
+        return "/project/create";
+    }
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
+    }
+    @GetMapping("/delete/{code}")
+    public String deleteProject(@PathVariable("code") String code, Model model){
+        projectService.deleteById(code);
+        model.addAttribute("managers", userService.findAll());
+        model.addAttribute("projects",projectService.findAll());
+        return "redirect:/project/create";
+    }
+
+}
