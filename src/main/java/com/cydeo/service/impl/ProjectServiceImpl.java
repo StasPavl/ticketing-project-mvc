9d696b1df3df370737c77139cdc8d730ccea0da1
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-13 23:57:23 -0400

    project service layer created

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+    @Override
+    public ProjectDTO save(ProjectDTO project) {
+        return super.save(project.getCode(), project);
+    }
+
+    @Override
+    public ProjectDTO findById(String projectCode) {
+        return super.findById(projectCode);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String projectCode) {
+        super.deleteById(projectCode);
+    }
+
+    @Override
+    public void update(ProjectDTO object) {
+        super.update(object.getCode(),object);
+    }
+}
