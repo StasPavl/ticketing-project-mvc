eb4ebbae9267876e15753a3ec0974f6e74dd14a2
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-14 00:56:54 -0400

    created Converter for User From String to USERDTO

---
+++ src/main/java/com/cydeo/convertor/MangerConvertor.java
@@ -0,0 +1,25 @@
+package com.cydeo.convertor;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class MangerConvertor implements Converter<String, UserDTO> {
+
+    private final UserService userService;
+
+    public MangerConvertor(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
+}
