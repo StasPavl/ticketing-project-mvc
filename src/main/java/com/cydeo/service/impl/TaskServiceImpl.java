fb8584294ac7d7c63fcedb50a9b6717bd297e6ab
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-16 21:57:39 -0400

    task service implementation created

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,46 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
+    @Override
+    public TaskDTO save(TaskDTO task) {
+        if(task.getAssignedDate() == null){
+            task.setAssignedDate(LocalDate.now());
+        }
+        task.setTaskStatus(Status.IN_PROGRESS);
+        if(task.getId()==null)
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        return super.save(task.getId(),task);
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<TaskDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+    }
+
+    @Override
+    public void update(TaskDTO object) {
+
+        super.update(object.getId(), object);
+    }
+}
