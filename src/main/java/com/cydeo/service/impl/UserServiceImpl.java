3e7e3c05a313381c876e5c26bb55221d2d8ba50b
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-16 21:59:17 -0400

    findEmployee method added and implementation added

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -38,4 +38,9 @@ public class UserServiceImpl extends AbstractMapService<UserDTO,String> implemen
     public List<UserDTO> findManagers() {
         return findAll().stream().filter(u -> u.getRole().getId() == 2).collect(Collectors.toList());
     }
+
+    @Override
+    public List<UserDTO> findEmployee() {
+        return findAll().stream().filter(u -> u.getRole().getId() ==3).collect(Collectors.toList());
+    }
 }
