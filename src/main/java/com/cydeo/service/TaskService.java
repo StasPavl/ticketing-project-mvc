dd4284a80d935e0a112f892ab8da69955bf52d27
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-25 21:38:45 -0400

    added findAllTasksByStatusIsNot method and all implementation for this method

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -2,10 +2,12 @@ package com.cydeo.service;
 
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
 public interface TaskService extends CRUDService<TaskDTO,Long>{
 
     List<TaskDTO> findTasksByManager(UserDTO manager);
+    List<TaskDTO> findAllTasksByStatusIsNot(Status status);
 }
