43db507904251aeea36101c7536729c1ab516d7d
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-25 21:05:41 -0400

    add getCountListOfProjectsDTO method

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -1,8 +1,13 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
 
 public interface ProjectService extends CRUDService<ProjectDTO,String> {
 
     void complete(ProjectDTO project);
+
+    List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager);
 }
