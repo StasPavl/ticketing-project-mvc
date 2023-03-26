4b89658bf63a85d7b00bee728792aaa12007db33
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-25 21:03:59 -0400

    create project status controller sorted by managers

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
@@ -72,6 +74,17 @@ public class ProjectController {
         projectService.update(project);
 
         return"redirect:/project/create";
+    }
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cydeo.com");//coming from log in page(security)
+        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
+
+        model.addAttribute("projects", projects);
+
+        return "/manager/project-status";
+
     }
 
 
